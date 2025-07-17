f5190fe2377a8e1f40aa2166ca520c83f66b6f55
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-07-17 23:30:26 +0530

    Add files via upload

---
+++ RoomRentalSagar/RoomRentalSagar/src/test/java/RoomRentalSagar/RoomRentalSagar/RoomRentalSagarApplicationTests.java
@@ -0,0 +1,13 @@
+package RoomRentalSagar.RoomRentalSagar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomRentalSagarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
