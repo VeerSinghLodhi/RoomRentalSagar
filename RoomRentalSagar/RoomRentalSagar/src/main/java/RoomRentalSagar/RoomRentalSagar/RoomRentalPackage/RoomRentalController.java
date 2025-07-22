140917422f50c32ba84330b3f06a08079a9b3e1d
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-07-22 21:02:12 +0530

    Update RoomRentalController.java

--- RoomRentalSagar/RoomRentalSagar/src/main/java/RoomRentalSagar/RoomRentalSagar/RoomRentalPackage/RoomRentalController.java
+++ RoomRentalSagar/RoomRentalSagar/src/main/java/RoomRentalSagar/RoomRentalSagar/RoomRentalPackage/RoomRentalController.java
@@ -1,4 +1,4 @@
-package RoomRentalSagar.RoomRentalSagar.RoomRentalPackage;
+package RoomRentalSagar.RoomRentalSagar.RoomOwnersPackage;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,25 +7,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.Optional;
 
 @Controller
-public class RoomRentalController {
+public class RoomOwnersController {
     @Autowired
-    RoomRentalRepository roomRentalRepository;
+    RoomOwnersRepository roomRentalRepository;
 
     @GetMapping("/signup")
     public String getSignUp(Model model){
-        model.addAttribute("roomrental",new RoomRental());
+        model.addAttribute("roomowner",new RoomOwners());
         return "Forms/signup";
     }
 
     @PostMapping("/register")
-    public String getRegister(@ModelAttribute("roomrental") RoomRental roomRental,Model model){
+    public String getRegister(@ModelAttribute("roomrental") RoomOwners roomRental, Model model){
 
        if(roomRentalRepository.findByEmail(roomRental.getEmail()).isPresent()){
             model.addAttribute("alreadyexists","Email Already Exists");
@@ -33,35 +31,35 @@ public class RoomRentalController {
         }
 
 
-        RoomRental newRoomRental=new RoomRental();
-        newRoomRental.setFirstname(roomRental.getFirstname());
-        newRoomRental.setLastname(roomRental.getLastname());
-        newRoomRental.setEmail(roomRental.getEmail());
-        newRoomRental.setPassword(roomRental.getPassword());
-        newRoomRental.setPhonenumber(roomRental.getPhonenumber());
-        newRoomRental.setStatus('Y');
-        newRoomRental.setJoiningdate(new Date());
-        roomRentalRepository.save(newRoomRental);
+        RoomOwners roomOwners=new RoomOwners();
+        roomOwners.setFirstname(roomRental.getFirstname());
+        roomOwners.setLastname(roomRental.getLastname());
+        roomOwners.setEmail(roomRental.getEmail());
+        roomOwners.setPassword(roomRental.getPassword());
+        roomOwners.setPhonenumber(roomRental.getPhonenumber());
+        roomOwners.setStatus('Y');
+        roomOwners.setJoiningdate(new Date());
+        roomOwners.setProfile_pic(null);
+        roomOwners.setAddress(null);
+        roomRentalRepository.save(roomOwners);
         model.addAttribute("after_register","Registration Successful");
         return "Forms/login";
     }
 
-    @GetMapping("/checkuser")
+    @GetMapping("/dashboard")
     public String getDashboard(String email, String password, HttpSession session, Model model){
-        Optional<RoomRental> roomRental=roomRentalRepository.findByEmailAndPassword(email,password);
+        Optional<RoomOwners> roomRental=roomRentalRepository.findByEmailAndPassword(email,password);
         System.out.println("Inside the Check User");
         if(roomRental.isPresent()){
             session.setAttribute("roomRental",roomRental.get());
             model.addAttribute("roomRental",roomRental.get());
             return "Forms/dashboard";
         }
+        model.addAttribute("email",email);
+        model.addAttribute("password",password);
         model.addAttribute("invalid","Invalid credential");
         return "Forms/login";
     }
 
-    @GetMapping("/checkemail")
-    public String getCheckEmail(String email){
-
-        return "";
-    }
+   
 }
