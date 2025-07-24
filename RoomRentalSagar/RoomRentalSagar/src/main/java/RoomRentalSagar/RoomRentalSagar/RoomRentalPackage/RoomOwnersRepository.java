785d6ecc415c947736fc70d6ca519a8fbb28d38a
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-07-24 22:32:12 +0530

    Update RoomOwnersRepository.java

--- RoomRentalSagar/RoomRentalSagar/src/main/java/RoomRentalSagar/RoomRentalSagar/RoomRentalPackage/RoomOwnersRepository.java
+++ RoomRentalSagar/RoomRentalSagar/src/main/java/RoomRentalSagar/RoomRentalSagar/RoomRentalPackage/RoomOwnersRepository.java
@@ -1,6 +1,10 @@
 package RoomRentalSagar.RoomRentalSagar.RoomOwnersPackage;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +15,10 @@ public interface RoomOwnersRepository extends JpaRepository<RoomOwners,Integer> 
      Optional<RoomOwners> findByEmailAndPassword(String email, String password);
 
      Optional<RoomOwners> findByEmail(String email);
+
+     @Query("update RoomOwners rw set rw.password =:newPassword where rw.email=:email")
+     @Transactional
+     @Modifying
+     void getUpdatePassword(@Param("email") String email,@Param("newPassword") String newPassword);
+
 }
